454755b40a63b535aa8ca2caab7e4fe8f22d0e25
Shane McDonough <shanemcdman@gmail.com>
2020-06-28 23:16:17 -0700

    add non-binary flag

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-const FLAGS =["lgbt", "bi", "pan", "trans", "lesbian"]
+const FLAGS =["lgbt", "bi", "pan", "trans", "lesbian", "nb"]
 
 async function sleep(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
